006f072ea424a46e7c93a4734816a220d444faa8
shitzhang <529190912@qq.com>
2021-10-14 16:04:12 +0800

    add project files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Tencent UE4 class project
+# Tencent UE4 class project  
 
-homework1:
-apk and mp4 link: https://pan.baidu.com/s/1--pp9xqlZ5xbfvvLfN9Evw 
-Extraction code: xb6f 
+homework1:  
+apk and mp4 link: https://pan.baidu.com/s/1--pp9xqlZ5xbfvvLfN9Evw  
+Extraction code: xb6f  
---
+++ Source/prototype/prototypeGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "prototypeGameMode.h"
+#include "prototypeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AprototypeGameMode::AprototypeGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
